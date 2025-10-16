6ee6d185667c50c207bf53bb054d90d9dc4bac38
VojtechKr <2005kratochvil@seznam.cz>
2025-10-16 09:36:49 +0200

    Cviceni 5

---
+++ Cv05/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,55 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+void soucet(double a[3], double b[3], double c[3]) {
+	for (int i = 0; i < 3; i++) {
+		c[i] = a[i] + b[i];
+	}
+}
+
+double velikost(double a[3]) {
+	return sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+}
+
+
+double skalarniSoucin(double a[3], double b[3]) {
+	return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+}
+
+
+void vektorovySoucin(double a[3], double b[3], double c[3]) {
+	c[0] = a[1]*b[2] - a[2]*b[1];
+	c[1] = a[2]*b[0] - a[0]*b[2];
+	c[2] = a[0]*b[1] - a[1]*b[0];
+}
+
+int main()
+{
+	double a[3], b[3], c[3];
+
+	cout << "Vektor A (3 hodnoty oddelene carkou); ";
+		char sep;
+	cin >> a[0] >> sep >> a[1] >> sep >> a[2];
+
+	cout << "Vektor B (3 hodnoty oddelene carkou) ";
+	cin >> b[0] >> sep >> b[1] >> sep >> b[2];
+
+
+	soucet(a, b, c);
+	cout << "Soucet vektoru A+B je: (" << c[0] << ", " << c[1] << ", " << c[2] << ")" << endl;
+
+
+	cout << "Velikost vektoru A je: " << velikost(a) << endl;
+
+	cout << "Skalarni soucin A*B je: " << skalarniSoucin(a,b) << endl;
+
+
+	vektorovySoucin(a, b, c);
+	cout << "Vektorovy soucin A x B je: (" << c[0] << ", " << c[1] << ", " << c[2] << ")" << endl;
+
+	return 0;
+}
