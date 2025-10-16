5175754c26ea74164da49fc46c4380589bdabb63
VojtechKr <2005kratochvil@seznam.cz>
2025-10-16 09:52:27 +0200

    Cvičení 05 bod 2

---
+++ Cv05/Cvičení 05 bod 2/Cvičení 05 bod 2.h
@@ -0,0 +1,8 @@
+﻿// Cvičení 05 bod 2.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
